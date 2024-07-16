46a43d3a4e0fae6546234a6f5be64898c8852a59
kristel sofía Sancho herrera <Sofiasancho0219@gmail.com>
2024-07-16 17:27:51 -0600

    PRUEBA1SOFI

--- CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
+++ CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
@@ -9,5 +9,10 @@ public class Practica1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1Application.class, args);
 	}
+        
+        
+        
+        
+        
 
 }
