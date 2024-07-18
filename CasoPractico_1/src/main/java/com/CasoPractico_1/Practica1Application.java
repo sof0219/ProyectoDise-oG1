177a4031244b317ea805d55675bece65e3129ed0
MiguelOtarola <miguel.otarola03@gmail.com>
2024-07-17 21:29:04 -0600

    Miguel

--- CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
+++ CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
@@ -9,6 +9,7 @@ public class Practica1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1Application.class, args);
                 //Hola grupo esto es una prueba para el push Isaac C.
+                // Helloooooooo Miguel
 	}
         
         
