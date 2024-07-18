a060cd6fe12334cb7e213cf0fdeee3da6227073e
iConejis <isaacconejo22@gmail.com>
2024-07-17 21:24:13 -0600

    Prueba de push Isaac

--- CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
+++ CasoPractico_1/src/main/java/com/CasoPractico_1/Practica1Application.java
@@ -8,6 +8,7 @@ public class Practica1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1Application.class, args);
+                //Hola grupo esto es una prueba para el push Isaac C.
 	}
         
         
