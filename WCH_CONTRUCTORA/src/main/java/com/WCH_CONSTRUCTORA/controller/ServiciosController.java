23f18596b5a97df19a94eee00fb3f5ccca46101a
Nathalie Poveda <npoveda30053@ufide.ac.cr>
2024-07-19 01:15:40 -0600

    Tabs dentro de la Pagina de Servicios

--- WCH_CONTRUCTORA/src/main/java/com/WCH_CONSTRUCTORA/controller/ServiciosController.java
+++ WCH_CONTRUCTORA/src/main/java/com/WCH_CONSTRUCTORA/controller/ServiciosController.java
@@ -1,6 +1,10 @@
 package com.WCH_CONSTRUCTORA.controller;
 
+import com.WCH_CONSTRUCTORA.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,6 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/servicios")
 public class ServiciosController {
+    @Autowired
+    private CategoriaService categoriaService;
     
-    
+    @GetMapping("/listado")
+    public String listado(Model model){
+        var categorias = categoriaService.getCategorias(); 
+
+        model.addAttribute("categorias",categorias);
+
+        
+        return "/servicios/listado";
+    }
 }
