f94b89182ffefe20f1623c3d7f25f067bc656abe
iConejis <isaacconejo22@gmail.com>
2024-08-08 14:29:22 -0600

    Registro funciona, el correo se envia y se puede agregar los datos y quedan agregados a la base Borre el registro duplicado

--- WCH_CONTRUCTORA/src/main/java/com/WCH_CONSTRUCTORA/controller/RegistroUserController.java
+++ WCH_CONTRUCTORA/src/main/java/com/WCH_CONSTRUCTORA/controller/RegistroUserController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.WCH_CONSTRUCTORA.controller;
 
 import com.WCH_CONSTRUCTORA.domain.Usuario;
